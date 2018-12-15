3c530ba77f572828109ff132964f37346ab0a241
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2018-12-15 14:20:58 +0900

    Render react-native-maps

--- App.js
+++ App.js
@@ -8,49 +8,37 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios:
-    'iOS と Android で表示の出し分けができそうです。\n' +
-    'Cmd+R キーでリロードしたり、 Cmd+D キーでメニューを出したりできるとのこと。',
-  android:
-    'ここは Android 用なので、編集しても iOS では表示されなさそう。,\n' +
-    'せっかくなのでスタイルシートもちょっと変えてみます。',
-});
+import MapView from 'react-native-maps';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>ここに地図を出したい</Text>
-        <Text style={styles.instructions}>OpenStreetMapを使ってみたい</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+      </MapView>
+    </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    ...StyleSheet.absoluteFillObject,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#ffffff',
   },
-  welcome: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: '#333333',
-    textAlign: 'center',
-    marginBottom: 40,
-  },
-  instructions: {
-    fontSize: 14,
-    lineHeight: 20,
-    textAlign: 'left',
-    color: '#666666',
-    marginHorizontal: 30,
-    marginBottom: 30,
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
