caadcacd0fb91337bc9b40b475b465e30524a798
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2018-12-15 15:01:54 +0900

    Extract region as state

--- App.js
+++ App.js
@@ -12,17 +12,24 @@ import MapView from 'react-native-maps';
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: 35.645736,
+        longitude: 139.747575,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.03,
+      },
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <MapView
           style={styles.map}
-          region={{
-            latitude: 35.645736,
-            longitude: 139.747575,
-            latitudeDelta: 0.03,
-            longitudeDelta: 0.03,
-          }}
+          region={this.state.region}
         >
         </MapView>
       </View>
